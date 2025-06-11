d2afe5b64e7bd0721bb15b6baf927f4fff9cb41b
yTekipS <rafalburek50@gmail.com>
2025-06-11 12:06:36 +0200

    Simple Raylib Template. With player control and camera following

--- main.cpp
+++ main.cpp
@@ -2,36 +2,53 @@
 
 int main(void)
 {
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
-    const Vector3 CubePlace = {640.0f,360.0f,0.0f};
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    Camera3D camera = {0};
+    InitWindow(screenWidth, screenHeight, "Simple Raylib Temple.");
 
-    InitWindow(screenWidth, screenHeight, "Raylib");
+    Rectangle player = {400.0f, 285.0f, 50.0f, 50.0f};
 
-    SetTargetFPS(60);        
+    Camera2D camera = {0};
+    camera.target = (Vector2){player.x + 20.0f, player.y + 20.0f};
+    camera.offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+    SetTargetFPS(60);
 
     //* MAIN GAME LOOP
-    while (!WindowShouldClose())  
+    while (!WindowShouldClose())
     {
-        //* UPDATE
+        //* UPDATEa
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
+        if (IsKeyDown(KEY_W))
+            player.y -= 2;
+        else if (IsKeyDown(KEY_S))
+            player.y += 2;
+        if (IsKeyDown(KEY_A))
+            player.x -= 2;
+        else if (IsKeyDown(KEY_D))
+            player.x += 2;
+
+        camera.target = (Vector2){player.x + 20.0f, player.y + 20.0f};
         //----------------------------------------------------------------------------------
 
         //* DRAW
         //----------------------------------------------------------------------------------
         BeginDrawing();
-
-            DrawCube(CubePlace,100.0f,100.0f,0.0f,BLUE);
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
+        BeginMode2D(camera);
+            DrawCircle(400.0f, 285.0f, 40.0f, RED);
+            DrawRectangleRec(player, BLUE);
+        EndMode2D();
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
-    CloseWindow();        
+    CloseWindow();
 
     return 0;
 }
