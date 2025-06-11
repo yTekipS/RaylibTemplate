339ac3a03b5ea2b7ce23d3c95288145482a7a6c0
yTekipS <rafalburek50@gmail.com>
2025-06-11 09:26:00 +0200

    Comenting template

--- main.cpp
+++ main.cpp
@@ -1,57 +1,24 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Example complexity rating: [★☆☆☆] 1/4
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute 'raylib_compile_execute' script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   To test the examples on Web, press F6 and execute 'raylib_compile_execute_web' script
-*   Web version of the program is generated in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2025 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+    // DEFINING SCREEN SIZE
     const int screenWidth = 800;
     const int screenHeight = 450;
-
+    // INITIALAIZING WINDOW
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(60);  // SETTING TARGETED AMOUNT OF FPS        
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    // MAIN GAME LOOP
+    while (!WindowShouldClose())  
     {
-        // Update
+        // UPDATE
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
 
-        // Draw
+        // DRAW
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
@@ -63,10 +30,7 @@ int main(void)
         //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();        // CLOSE WINDOW AND OPENGL CONTEXT
 
     return 0;
 }
